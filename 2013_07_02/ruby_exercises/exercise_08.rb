1845ad0a05551c2edcdfdffb45e49a0f349fe061
Cole Busby <colebusby@gmail.com>
2013-07-01 17:58:01 -0700

    Ruby exercises

---
+++ 2013_07_02/ruby_exercises/exercise_08.rb
@@ -0,0 +1,2 @@
+hash_old = {:a => "The Wii", :b => "totally", :c => "pwns", :d => "Xbox and PS3"}
+hash_new = {a: "The Wii", b: "totally", c: "pwns", d: "Xbox and PS3"}
---
+++ 2013_07_02/ruby_exercises/exercise_13.rb
@@ -0,0 +1,2 @@
+h = {a:1, b:2, c:3, d:4, e:5}
+h.delete_if { |key, value| value < 3.5 }
